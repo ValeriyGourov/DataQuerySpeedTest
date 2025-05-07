723626fb98c7a30818433da960d1486589156239
Валерий Гуров <Valeriy.Gourov@outlook.com>
2025-05-07 18:32:10 +0300

    Реализация для сервера возможности работать с gRPC

--- Server/Program.cs
+++ Server/Program.cs
@@ -1,4 +1,6 @@
-﻿using Server.QueryBuilders;
+﻿using ProtoBuf.Grpc.Server;
+
+using Server.QueryBuilders;
 
 WebApplicationBuilder builder = WebApplication.CreateBuilder(args);
 
@@ -6,16 +8,29 @@ builder.AddServiceDefaults();
 
 builder.Services.AddMediator();
 
+builder.Services.AddCodeFirstGrpc(options
+	=> options.ResponseCompressionLevel = System.IO.Compression.CompressionLevel.Optimal);
+
+if (builder.Environment.IsDevelopment())
+{
+	builder.Services.AddCodeFirstGrpcReflection();
+}
+
 WebApplication app = builder.Build();
 
 app.MapDefaultEndpoints();
-
-app.UseHttpsRedirection();
 
 app.UseWebSockets();
 
 app
 	.MapRestEndpoints()
 	.MapWebSocketEndpoints();
+
+if (app.Environment.IsDevelopment())
+{
+	app.MapCodeFirstGrpcReflectionService();
+}
+
+app.MapGrpcService<GrpcService>();
 
 await app.RunAsync().ConfigureAwait(false);
